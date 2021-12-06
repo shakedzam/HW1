90ebe18f4be00401c294f80e156e163249cea2ad
Liran <liranc6@campus.technion.ac.il>
2021-12-06 18:50:51 +0000

    change

---
+++ cmake-build-debug/CMakeFiles/untitled.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/student/Documents/HW1.1/Commands.cpp" "CMakeFiles/untitled.dir/Commands.cpp.o" "gcc" "CMakeFiles/untitled.dir/Commands.cpp.o.d"
+  "/home/student/Documents/HW1.1/signals.cpp" "CMakeFiles/untitled.dir/signals.cpp.o" "gcc" "CMakeFiles/untitled.dir/signals.cpp.o.d"
+  "/home/student/Documents/HW1.1/smash.cpp" "CMakeFiles/untitled.dir/smash.cpp.o" "gcc" "CMakeFiles/untitled.dir/smash.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
